b2f157ce535136697367e88844acb340c342adf9
Moltenship <moltenship7750@gmail.com>
2021-05-26 00:28:36 +0700

    Firebase config file

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default firebaseConfig = {
+  apiKey: 'AIzaSyA21y5o2PjkBD4O47DXRp07otfvUtYvuzI',
+  authDomain: 'book-catalog-2ccd2.firebaseapp.com',
+  projectId: 'book-catalog-2ccd2',
+  storageBucket: 'book-catalog-2ccd2.appspot.com',
+  messagingSenderId: '1051865204183',
+  appId: '1:1051865204183:web:03490dcb3357d947dcf792',
+  measurementId: 'G-YYG5DJV0LM',
+}
