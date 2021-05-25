ca9924c7f91027d05af177c9f405bd8281a79593
Timofey Maximov <40624720+Moltenship@users.noreply.github.com>
2021-05-26 00:32:08 +0700

    Delete firebaseConfig.js

--- src/utils/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default firebaseConfig = {
-  apiKey: 'AIzaSyA21y5o2PjkBD4O47DXRp07otfvUtYvuzI',
-  authDomain: 'book-catalog-2ccd2.firebaseapp.com',
-  projectId: 'book-catalog-2ccd2',
-  storageBucket: 'book-catalog-2ccd2.appspot.com',
-  messagingSenderId: '1051865204183',
-  appId: '1:1051865204183:web:03490dcb3357d947dcf792',
-  measurementId: 'G-YYG5DJV0LM',
-}
